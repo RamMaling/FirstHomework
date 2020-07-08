fcb32209dfee8ba54294058398ef5ce7f877a83d
RamMaling <68011245+RamMaling@users.noreply.github.com>
2020-07-08 18:37:43 +0700

    Add files via upload

---
+++ unit2_ex_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void main(){
+
+	cout << "   #" << endl;
+	cout << "  # #" << endl;
+	cout << " #   #" << endl;
+	cout << "#     #" << endl;
+	cout << "#######" << endl;
+	cout << "#     #" << endl;
+	cout << endl;
+	cout << "Chitsanuphong Chaihong";
+	getchar();
+}
